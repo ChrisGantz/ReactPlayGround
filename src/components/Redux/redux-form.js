23f143ef81ad0c43ff183f8d73760e1c9f75cf0f
Chris Gantz <chris.gonzalez836@gmail.com>
2019-01-29 14:26:05 -0700

    form css edits

--- src/components/Redux/redux-form.js
+++ src/components/Redux/redux-form.js
@@ -3,34 +3,52 @@ import React from 'react';
 class ReduxForms extends React.Component {
   render() {
     return (
-      <div className="center-item">
+      <div className="center-item-form">
         <h1 style={{order: 0}}>Survey Form</h1>
         <div className="form-container">
             <p>Let us know how to improve! <span role="img" aria-live="polite" >👏</span></p>
           <form>
-            <div>
+            <div className="form-divs">
+              <div className="label">
               <label for="name">Name:</label>
+            </div>
+            <div>
               <input type="text" name="name" placeholder="John Doe" />
             </div>
-            <div>
+            </div>
+            <div className="form-divs">
+              <div className="label">
               <label for="email">Email</label>
+              </div>
+              <div>
               <input type="text" name="email" placeholder="johnDoe3@email.com" />
+              </div>
             </div>
-            <div>
-              <label>Age</label>
+            <div className="form-divs">
+              <div className="label">
+              <label >Age</label>
+              </div>
+              <div>
               <input type="number" name="age" placeholder="21" />
+              </div>
             </div>
-            <div>
-              <label>What is your current role?</label>
+            <div className="form-divs">
+              <div className="label">
+                <label>What is your current role?</label>
+              </div>
+              <div>
               <select name="occupation">
                 <option value="student">Student</option>
                 <option value="full-time">Full Time Job</option>
                 <option value="part-time">Part Time Job</option>
                 <option value="other">Other</option>
               </select>
+              </div>
             </div>
-            <div>
+            <div className="form-divs">
+              <div className="label">
               <label for="radio">Would you recommend us?</label>
+              </div>
               <ul>
                 <li>
                   <label for="yes">Yes</label>
@@ -46,8 +64,10 @@ class ReduxForms extends React.Component {
                 </li>
               </ul>
             </div>
-            <div>
-              <label>What can we improve</label>
+            <div className="form-divs">
+              <div className="label">
+                <label>What can we improve</label>
+              </div>
               <ul>
                 <li>
                   <label>
@@ -81,8 +101,10 @@ class ReduxForms extends React.Component {
                 </li>
               </ul>
             </div>
-            <div>
+            <div className="form-divs">
+              <div className="label">
               <label for="comments">Comments or Suggestions?</label>
+              </div>
               <input type="textbox" name="comments" />
             </div>
           </form>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from "react-router-dom";
-import './css-components/main.css';
-
+// CSS
+import "./css-components/main.css";
+import "./css-components/form.css";
 
 ReactDOM.render(
   <Router>
